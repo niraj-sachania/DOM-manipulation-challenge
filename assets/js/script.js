82ee4cf994347ca0035d4720d68441e62049eba2
niraj-sachania <>
2025-11-07 16:09:00 +0000

    feat: add JS for adding list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
 // Select DOM elements
-const input = 
-const addBtn = 
-const list = 
+const input = document.querySelector('#itemInput');
+const addBtn = document.querySelector('#addBtn');
+const list = document.querySelector('#list');
 
 // Add event listener for button click
+addBtn.addEventListener('click', function() {
 
-  if (text !== '') {
+    const inputValue = input.value;
+
+    if (inputValue !== '') {
     // 1️. Create new <li>
+    const li = document.createElement("li");
+    li.innerText = inputValue;
 
     // 2️. Append to <ul>
+    list.append(li);
 
     // 3️. Clear input
+    input.innerText = '';
 
   } else {
     alert('Please enter a valid item!');
   };
+})
+
+
