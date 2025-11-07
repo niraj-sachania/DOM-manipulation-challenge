ff4e92c15343bf5450ec48e192e5eccb007af174
tomcowtutor <tom.cowen@codeinstitute.net>
2025-11-07 13:46:30 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Select DOM elements
+const input = 
+const addBtn = 
+const list = 
+
+// Add event listener for button click
+
+  if (text !== '') {
+    // 1️. Create new <li>
+
+    // 2️. Append to <ul>
+
+    // 3️. Clear input
+
+  } else {
+    alert('Please enter a valid item!');
+  };
